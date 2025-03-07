2c8b2eee9ce9fc9d5f6d1b0724b9ba525e68b0b5
Binny <patelbinny884@gmail.com>
2025-03-07 16:04:49 +0100

    Corrected creation queries

--- src/main/resources/mySQL/dataCreation.sql
+++ src/main/resources/mySQL/dataCreation.sql
@@ -1,14 +1,26 @@
+CREATE TABLE Team (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    country VARCHAR(255) NOT NULL,
+    cityName VARCHAR(255) NOT NULL,
+    stadiumName VARCHAR(255) NOT NULL,
+    establishedDate DATE
+);
 CREATE TABLE Player (
     id INT PRIMARY KEY AUTO_INCREMENT,
     jerseyNo INT NOT NULL,
     fullName VARCHAR(255) NOT NULL,
     appearances INT,
     goals INT,
-    FOREIGN KEY (team_id) REFERENCES Team(id)
-
-    	private Integer jerseyNo;
-	private String fullName;
-	private int appearances;
-	private int goals;
-	private long wageInPounds;
+    teamId INT,
+    FOREIGN KEY (teamId) REFERENCES Team(id)
+);
+CREATE TABLE Staff(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    contractExpiry DATE,
+    salary BIGINT,
+    education VARCHAR(255),
+    teamId INT,
+    FOREIGN KEY (teamId) REFERENCES Team(id)
 );
