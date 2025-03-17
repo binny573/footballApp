eb666062ca95b76a7fe9ec20297d1ed8ff63f954
Binny <patelbinny884@gmail.com>
2025-03-17 14:06:48 +0100

    Can fetch data using for both staff and player services

--- src/main/java/com/footballapp/footballapp/entity/Player.java
+++ src/main/java/com/footballapp/footballapp/entity/Player.java
@@ -2,57 +2,75 @@ package com.footballapp.footballapp.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "Player")
 public class Player {
-	
+
 	@Id
-	private Integer jerseyNo;
-	private String fullName;
-	private int appearances;
-	private int goals;
-	private long wageInPounds;
-		
-//	@ConstructorProperties(value = { "jerseyNo", "fullName", "appearances", "goals", "wageInPounds" })
-//	public Player(Integer jerseyNo, String fullName, int appearances, int goals, long wageInPounds) {
+	private Integer id;
+	private Integer jerseyno;
+	private String fullname;
+	private Integer appearances;
+	private Integer goals;
+	private Integer teamid;
+	// private long wageinpounds;
+
+//	@ConstructorProperties(value = { "jerseyNo", "fullname", "appearances", "goals", "wageinpounds" })
+//	public Player(Integer jerseyNo, String fullname, int appearances, int goals, long wageinpounds) {
 //		super();
 //		this.jerseyNo = jerseyNo;
-//		this.fullName = fullName;
+//		this.fullname = fullname;
 //		this.appearances = appearances;
 //		this.goals = goals;
-//		this.wageInPounds = wageInPounds;
+//		this.wageinpounds = wageinpounds;
 //	} This created the problem , now it works haha aha 
-	
+
 	//Alt+shift+S, r,....space down, space down
-	
-	public String getFullName() {
-		return fullName;
+
+	public Integer getId() {
+		return id;
 	}
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
+	public void setId(Integer id) {
+		this.id = id;
 	}
-	public int getAppearances() {
+
+	public Integer getTeamId() {
+		return teamid;
+	}
+	public void setTeamId(Integer teamid) {
+		this.teamid = teamid;
+	}
+
+	public String getfullname() {
+		return fullname;
+	}
+	public void setfullname(String fullname) {
+		this.fullname = fullname;
+	}
+	public Integer getAppearances() {
 		return appearances;
 	}
-	public void setAppearances(int appearances) {
+	public void setAppearances(Integer appearances) {
 		this.appearances = appearances;
 	}
-	public int getGoals() {
+	public Integer getGoals() {
 		return goals;
 	}
-	public void setGoals(int goals) {
+	public void setGoals(Integer goals) {
 		this.goals = goals;
 	}
-	public long getWageInPounds() {
-		return wageInPounds;
-	}
-	public void setWageInPounds(long wageInPounds) {
-		this.wageInPounds = wageInPounds;
-	}
+	// public long getWageInPounds() {
+	// 	return wageinpounds;
+	// }
+	// public void setWageInPounds(long wageinpounds) {
+	// 	this.wageinpounds = wageinpounds;
+	// }
 	public Integer getJerseyNo() {
-		return jerseyNo;
+		return jerseyno;
 	}
-	public void setJerseyNo(Integer jerseyNo) {
-		this.jerseyNo = jerseyNo;
+	public void setJerseyNo(Integer jerseyno) {
+		this.jerseyno = jerseyno;
 	}
 }
--- src/main/java/com/footballapp/footballapp/entity/Staff.java
+++ src/main/java/com/footballapp/footballapp/entity/Staff.java
@@ -4,7 +4,6 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Column;
 
 @Entity
 public class Staff {
